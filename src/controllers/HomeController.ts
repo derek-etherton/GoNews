73793c35ef6e5359f9ef18997493c8f33f06830d
derek-etherton <derek.etherton@mail.utoronto.ca>
2020-11-19 22:06:53 -0800

    :construction: Created Postgres database and configured app connection

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -2,6 +2,7 @@ import { Request } from "express";
 import IRequestController from './IRequestController';
 import WebScraper from '../model/scrapers/WebScraper';
 import SinaParser from '../model/parsers/SinaParser';
+import pool from '../model/db/config';
 
 class HomeController implements IRequestController {
     url: string;
@@ -16,6 +17,10 @@ class HomeController implements IRequestController {
 
         let pageHtml = await scraper.scrape(this.url);
         let articles = sinaParser.parse(pageHtml);
+
+        pool.query('SELECT * FROM articles', (error: any, results: any) => {
+            console.log(results);
+        });
 
         return articles;
     }
---
+++ src/model/db/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE articles (
+    ID SERIAL PRIMARY KEY,
+    url VARCHAR(255) UNIQUE NOT NULL,
+    title VARCHAR(80) NOT NULL,
+    source VARCHAR(20) NOT NULL,
+    date DATE,
+    author VARCHAR(100)
+);
