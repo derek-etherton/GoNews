bc263f01719c734c5bf9724270899412826d60b5
derek-etherton <derek.etherton@mail.utoronto.ca>
2020-11-28 23:38:39 -0800

    :truck: Moved web scraper api to /scrape endpoint, and started on json data for scrape job

--- src/controllers/HomeController.ts
+++ src/controllers/HomeController.ts
@@ -1,40 +1,12 @@
 import { Request } from "express";
 import IRequestController from './IRequestController';
-import WebScraper from '../model/scrapers/WebScraper';
-import SinaParser from '../model/parsers/SinaParser';
-import { postArticles } from '../model/db/dbHelpers';
-import AsahiParser from "../model/parsers/AsahiParser";
 
 class HomeController implements IRequestController {
-    url: string;
-    source: string;
-
     constructor(public request: Request) {
-        this.url = request.body.url;
-        this.source = request.body.source;
     }
 
     public async getResponse() {
-        let scraper = new WebScraper();
-        let pageParser;
-
-        switch (this.source.toUpperCase()) {
-            case "ASAHI":
-                pageParser = new AsahiParser();
-                break;
-            case "SINA":
-                pageParser = new SinaParser();
-                break;
-            default:
-                throw new Error("Invalid source specified");
-        }
-
-        let pageHtml = await scraper.scrape(this.url);
-        let articles = await pageParser.parse(pageHtml);
-
-        let addedArticles = await postArticles(articles);
-
-        return addedArticles;
+        return 'Healthy';
     }
 }
 
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,11 +1,19 @@
 import { Request, Response } from "express";
 import HomeController from "../controllers/HomeController";
+import ScrapeController from "../controllers/ScrapeController";
 
 const express = require('express');
 const router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
     let controller = new HomeController(req);
+    let response = await controller.getResponse();
+
+    res.send(response);
+});
+
+router.get('/scrape', async (req: Request, res: Response) => {
+    let controller = new ScrapeController(req);
     let response = await controller.getResponse();
 
     res.send(response);
